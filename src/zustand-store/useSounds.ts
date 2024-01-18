3bc3633cbbf7ec644f3f0cea90cb5940581b5f69
neerajjoshi-github <joshineeraj7037@gmail.com>
2024-01-18 12:34:09 +0530

    Presisting sound preferences in local storage

--- src/zustand-store/useSounds.ts
+++ src/zustand-store/useSounds.ts
@@ -1,5 +1,7 @@
 import { create } from "zustand";
 
+const sound_preferences_Key = "SOUND_PREFERENCES" as const;
+
 type soundsState = {
   errorSound: boolean;
   keySound: boolean;
@@ -7,9 +9,38 @@ type soundsState = {
   toggleKeySound: () => void;
 };
 
+type SoundPreferences = {
+  errorSound: boolean;
+  keySound: boolean;
+};
+
+const getUserSoundPreferences = (): SoundPreferences => {
+  const soundPreferences = localStorage.getItem(sound_preferences_Key);
+  if (!soundPreferences) return { errorSound: false, keySound: false };
+  return JSON.parse(soundPreferences) as SoundPreferences;
+};
+
+const setUserSoundPreferences = (soundPreferences: SoundPreferences) => {
+  localStorage.setItem(sound_preferences_Key, JSON.stringify(soundPreferences));
+};
+
 export const useSounds = create<soundsState>()((set) => ({
-  errorSound: false,
-  keySound: false,
-  toggleErrorSound: () => set((state) => ({ errorSound: !state.errorSound })),
-  toggleKeySound: () => set((state) => ({ keySound: !state.keySound })),
+  errorSound: getUserSoundPreferences().errorSound,
+  keySound: getUserSoundPreferences().keySound,
+  toggleErrorSound: () =>
+    set((state) => {
+      setUserSoundPreferences({
+        errorSound: !state.errorSound,
+        keySound: state.keySound,
+      });
+      return { errorSound: !state.errorSound };
+    }),
+  toggleKeySound: () =>
+    set((state) => {
+      setUserSoundPreferences({
+        errorSound: state.errorSound,
+        keySound: !state.keySound,
+      });
+      return { keySound: !state.keySound };
+    }),
 }));
